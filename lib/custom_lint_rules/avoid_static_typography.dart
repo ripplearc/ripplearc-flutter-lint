9ffe0a7eaf250d133783126830c42c07e1cdaa49
bazabizi <bazabizi3@gmail.com>
2025-12-03 19:49:04 +0300

    feat: Implement AvoidStaticTypography lint rule to enforce typography standards

---
+++ lib/custom_lint_rules/avoid_static_typography.dart
@@ -0,0 +1,13 @@
+import '../core/base_lint_rule.dart';
+import '../core/analyzers/avoid_static_typography_analyzer.dart';
+import '../core/analyzers/base_analyzer.dart';
+
+class AvoidStaticTypography extends BaseLintRule {
+  AvoidStaticTypography() : super(BaseLintRule.createLintCode(_analyzer));
+
+  static final _analyzer = AvoidStaticTypographyAnalyzer();
+
+  @override
+  BaseAnalyzer get analyzer => _analyzer;
+}
+
