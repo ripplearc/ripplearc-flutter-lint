d5b8112ea0d3157bd67e802e384ed6636c304c2b
bazabizi <bazabizi3@gmail.com>
2025-12-08 12:31:23 +0300

    Enable both files option in AvoidStaticTypography lint rule constructor

--- lib/custom_lint_rules/avoid_static_typography.dart
+++ lib/custom_lint_rules/avoid_static_typography.dart
@@ -3,7 +3,8 @@ import '../core/analyzers/avoid_static_typography_analyzer.dart';
 import '../core/analyzers/base_analyzer.dart';
 
 class AvoidStaticTypography extends BaseLintRule {
-  AvoidStaticTypography() : super(BaseLintRule.createLintCode(_analyzer));
+  AvoidStaticTypography()
+    : super(BaseLintRule.createLintCode(_analyzer), bothFiles: true);
 
   static final _analyzer = AvoidStaticTypographyAnalyzer();
 
