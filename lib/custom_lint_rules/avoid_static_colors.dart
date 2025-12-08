353307edbd5b949bad91831fbe274d6ba503cc64
bazabizi <bazabizi3@gmail.com>
2025-12-08 12:32:47 +0300

    Enable both files option in AvoidStaticColors lint rule constructor

--- lib/custom_lint_rules/avoid_static_colors.dart
+++ lib/custom_lint_rules/avoid_static_colors.dart
@@ -3,7 +3,8 @@ import '../core/analyzers/avoid_static_colors_analyzer.dart';
 import '../core/analyzers/base_analyzer.dart';
 
 class AvoidStaticColors extends BaseLintRule {
-  AvoidStaticColors() : super(BaseLintRule.createLintCode(_analyzer));
+  AvoidStaticColors()
+    : super(BaseLintRule.createLintCode(_analyzer), bothFiles: true);
 
   static final _analyzer = AvoidStaticColorsAnalyzer();
 
