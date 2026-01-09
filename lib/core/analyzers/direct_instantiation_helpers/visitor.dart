c5d7b33fb0a98b5c1c789a2ae3cdb0399a8efb97
bazabizi <bazabizi3@gmail.com>
2026-01-09 21:03:06 +0300

    feat: extend DirectInstantiationVisitor with method invocation analysis and refined exclusion checks

--- lib/core/analyzers/direct_instantiation_helpers/visitor.dart
+++ lib/core/analyzers/direct_instantiation_helpers/visitor.dart
@@ -11,7 +11,7 @@ class DirectInstantiationVisitor extends RecursiveAstVisitor<void> {
   final dynamic resolver;
   final String filePath;
   final List<LintIssue> issues = [];
-  final Function(InstanceCreationExpression) createIssue;
+  final Function(AstNode) createIssue;
 
   DirectInstantiationVisitor(
     this.createIssue, [
@@ -20,17 +20,117 @@ class DirectInstantiationVisitor extends RecursiveAstVisitor<void> {
   ]);
 
   @override
+  void visitMethodInvocation(MethodInvocation node) {
+    if (node.target == null && node.argumentList.arguments.isEmpty) {
+      final methodName = node.methodName.name;
+      final unit = node.root;
+      if (unit is CompilationUnit) {
+        for (final decl in unit.declarations) {
+          if (decl is ClassDeclaration && decl.name.lexeme == methodName) {
+            bool hasFactoryConstructor = false;
+            for (final member in decl.members) {
+              if (member is ConstructorDeclaration &&
+                  member.factoryKeyword != null) {
+                if (member.name == null || member.name!.lexeme == methodName) {
+                  hasFactoryConstructor = true;
+                  break;
+                }
+              }
+            }
+
+            if (!hasFactoryConstructor) {
+              _checkInstantiation(methodName, node);
+            }
+            break;
+          }
+        }
+      }
+    }
+    super.visitMethodInvocation(node);
+  }
+
+  void _checkInstantiation(String className, AstNode node) {
+    if (DirectInstantiationPatterns.isExcludedByFilePath(filePath) ||
+        BaseAnalyzer.isTestFile(filePath)) {
+      return;
+    }
+
+    if (_isInConstContext(node)) {
+      return;
+    }
+
+    if (_isInFactoryConstructor(node)) {
+      return;
+    }
+
+    if (DirectInstantiationPatterns.isExcludedByClassName(className)) {
+      return;
+    }
+
+    if (DirectInstantiationPatterns.isWhitelistedClassName(className)) {
+      return;
+    }
+
+    if (ContextChecker.isInsideModuleBindsMethod(node)) {
+      return;
+    }
+
+    if (ContextChecker.isInsideModule(node)) {
+      return;
+    }
+
+    if (ContextChecker.isExcludedClass(className, node)) {
+      return;
+    }
+
+    if (node is MethodInvocation) {
+      issues.add(createIssue(node));
+    }
+  }
+
+  bool _isInConstContext(AstNode node) {
+    AstNode? current = node.parent;
+    while (current != null) {
+      if (current is ListLiteral && current.constKeyword != null) return true;
+      if (current is SetOrMapLiteral && current.constKeyword != null)
+        return true;
+      if (current is VariableDeclaration) {
+        final parent = current.parent;
+        if (parent is VariableDeclarationList && parent.isConst) return true;
+      }
+      if (current is ConstructorDeclaration) break;
+      if (current is MethodDeclaration || current is FunctionDeclaration) break;
+      current = current.parent;
+    }
+    return false;
+  }
+
+  bool _isInFactoryConstructor(AstNode node) {
+    AstNode? current = node.parent;
+    while (current != null) {
+      if (current is ConstructorDeclaration) {
+        return current.factoryKeyword != null;
+      }
+      if (current is MethodDeclaration || current is FunctionDeclaration) break;
+      current = current.parent;
+    }
+    return false;
+  }
+
+  @override
   void visitInstanceCreationExpression(InstanceCreationExpression node) {
     final constructorName = node.constructorName;
-    final className = constructorName.type.name2.lexeme;
-    
-    if (DirectInstantiationPatterns.isExcludedByFilePath(filePath) || 
+    final name2 = constructorName.type.name2;
+    final className = name2.lexeme;
+
+    if (DirectInstantiationPatterns.isExcludedByFilePath(filePath) ||
         BaseAnalyzer.isTestFile(filePath)) {
       super.visitInstanceCreationExpression(node);
       return;
     }
 
-    if (ContextChecker.isExcludedByContext(node)) {
+    final isExcludedByContext = ContextChecker.isExcludedByContext(node);
+    if (isExcludedByContext) {
       super.visitInstanceCreationExpression(node);
       return;
     }
@@ -39,7 +139,7 @@ class DirectInstantiationVisitor extends RecursiveAstVisitor<void> {
       super.visitInstanceCreationExpression(node);
       return;
     }
-    
+
     if (DirectInstantiationPatterns.isWhitelistedClassName(className)) {
       super.visitInstanceCreationExpression(node);
       return;
@@ -47,11 +147,11 @@ class DirectInstantiationVisitor extends RecursiveAstVisitor<void> {
 
     if (constructorName.name != null) {
       bool shouldExclude = false;
-      
-      if (resolver != null && TypeChecker.isExcludedBySubtype(node)) shouldExclude = true;
-      if (!shouldExclude && resolver != null && TypeChecker.isSealedClass(node)) shouldExclude = true;
-      if (!shouldExclude && constructorName.name!.name.startsWith('_')) shouldExclude = true;
-      if (!shouldExclude && ImportChecker.isImportedFromModelClass(className, node)) shouldExclude = true;
+
+      if (constructorName.name!.name.startsWith('_')) shouldExclude = true;
+      if (!shouldExclude &&
+          ImportChecker.isImportedFromModelClass(className, node))
+        shouldExclude = true;
 
       if (shouldExclude) {
         super.visitInstanceCreationExpression(node);
